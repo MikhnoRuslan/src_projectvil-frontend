4722b9c786bf5b8c1ed8c50b505bcb8e1390b49d
Ruslan Mikhno <ruslan.mikhno@soxes.ch>
2024-06-24 18:40:13 +0200

    fix

--- projectvil/src/environments/environment.development.ts
+++ projectvil/src/environments/environment.development.ts
@@ -1,5 +1,8 @@
-const baseUrl = 'http://localhost:4200';
-const issuer = 'https://localhost:7014';
+//const baseUrl = 'http://localhost:4200';
+//const issuer = 'https://localhost:7014';
+
+const baseUrl = 'http://projectvil.somee.com';
+const issuer = 'http://projectvil.somee.com';
 
 export const environment = {
   production: false,
